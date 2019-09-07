bdb74c6dd6eadea252a79a9ad711132f265e8da1
ashutoshnautiyal04 <ashutoshnautiyal04@gmail.com>
2019-09-07 12:27:53 +0530

    first change

---
+++ ASHU.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+
+using namespace std;
+int main() {
+    int a=10;
+    cout<<"learning git and github";
+    cout<<"\n basic of git and github" ;
+    cout<<a;
+
+    return 0;
+}
